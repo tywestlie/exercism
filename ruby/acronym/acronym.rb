ae43902c092851866baa398bcbf9f354811c1c40
Tyler Westlie <tyler.westlie@gmail.com>
2018-12-06 18:04:40 -0700

    uses scan instead of iteration

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,9 +1,5 @@
 class Acronym
   def self.abbreviate(phrase)
-    split_phrase = phrase.split(/\W+/)
-    first_letter = split_phrase.map do |word|
-      word[0]
-    end
-    first_letter.join("").upcase
+    phrase.scan(/\b\w/).join.upcase
   end
 end
